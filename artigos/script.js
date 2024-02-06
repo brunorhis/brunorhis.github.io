3ac734c7308ca57ee6be3a58ac8e1cbb7b097359
Bruno Rhis <brunorhis@gmx.com>
2024-02-06 20:21:39 -0300

    Delete artigos/script.js

--- artigos/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    window.onscroll = function () {
-        // Lógica adicional relacionada ao scroll, se necessário.
-        // ...
-    };
-});
