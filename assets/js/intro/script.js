68edb793159e9dc607a4a94fbd1ed9cb7729e190
Bruno Rhis <brunorhis@gmx.com>
2024-02-10 17:54:34 -0300

    Update script.js

--- assets/js/intro/script.js
+++ assets/js/intro/script.js
@@ -50,7 +50,7 @@ document.addEventListener('DOMContentLoaded', function () {
             typeLetter();
         } else {
             setTimeout(() => {
-                outputElement.innerHTML += "<br>Acesso concedido, guest!";
+                outputElement.innerHTML += "<br>Acesso concedido, guest!<br>Redirecionando...";
 
                 setTimeout(() => {
                     if (window.location.href !== 'https://www.brcybersec.com/main.html') {
